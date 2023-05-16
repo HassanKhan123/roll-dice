87d6c78f34f6e12c1f78ba48c4b68c2d17875710
HassanKhan123 <hk2602809@gmail.com>
2023-05-16 18:53:52 +0500

    added: reusable data in widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:roll_dice/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colors, begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World!'),
       ),
     );
   }
